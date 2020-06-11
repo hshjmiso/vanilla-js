9651f98119151528a5d4dd0c18775a4ec8b2de82
hshjmiso <sw6297@gmail.com>
2020-06-12 00:40:01 +0900

    Using Fetch

--- nomad_coders/weather.js
+++ nomad_coders/weather.js
@@ -4,14 +4,35 @@ const API_KEY = "dd60d00e90a00df3ac0970191b7e0701";
 const COORDS = "coords";
 
 function getWeather(lat, lon) {
+    // default
+    /*
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
     .then(response => {
+        console.log(response);
         return response.json();
     })
     .then(json => {
         const temperature = json.main.temp;
         const place = json.name;
         weather.innerText = `${temperature} @ ${place}`;
+    });
+    */
+
+    // 성공 여부를 체크 일부러 *실패
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}22&units=metric`)
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error('Network response was not ok.');
+    })
+    .then(json => {
+        console.log(json);
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    }).catch(error => {
+        console.log(error.message);
     });
 }
 
